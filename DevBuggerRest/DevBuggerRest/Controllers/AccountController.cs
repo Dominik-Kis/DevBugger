212a2cd45ec2b95e96f178d2d7ae61dbc9befc96
IvanMatkovic <48958932+IvanMatkovic@users.noreply.github.com>
2022-04-06 23:14:55 +0200

    Add files via upload
    Rest s kojim radi get Accounts. Promijenite connection string u controlleru Account kako bi radilo i vama

---
+++ DevBuggerRest/DevBuggerRest/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevBuggerRest.Model;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DevBuggerRest.Controllers
+{
+    //http://localhost:5000/api/Account
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        public List<Account> GetAccounts()
+        {
+
+            //var con = ConfigurationManager.ConnectionStrings["MasterDatabase"].ToString();
+            string con = "Server=.\\SQLEXPRESS;Database=DevBugger;Trusted_Connection=True;";
+
+            List<Account> accounts = new List<Account>();
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                using (var command = new SqlCommand("selectAccounts", myConnection)
+                {
+                    CommandType = CommandType.StoredProcedure
+                })
+                {
+                    myConnection.Open();
+                    using (SqlDataReader oReader = command.ExecuteReader())
+                    {
+                        while (oReader.Read())
+                        {
+                            Account acc = new Account();
+                            acc.IDAccount = int.Parse(oReader["IDAccount"].ToString());
+                            acc.AccountLevelID = int.Parse(oReader["AccountLevelID"].ToString());
+                            acc.Email = oReader["Email"].ToString();
+                            acc.Username = oReader["Username"].ToString();
+                            acc.Password = oReader["Password"].ToString();
+                            acc.FirstName = oReader["FirstName"].ToString();
+                            acc.LastName = oReader["LastName"].ToString();
+                            acc.Created = DateTime.Parse(oReader["Created"].ToString());
+                            accounts.Add(acc);
+
+                        }
+
+                        myConnection.Close();
+                    }
+                }
+            }
+            return accounts;
+        }
+    }
+}
---
+++ DevBuggerRest/DevBuggerRest/Model/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace DevBuggerRest.Model
+{
+    [DataContract]
+    public class Account
+    {
+        [DataMember]
+        public int IDAccount { get; set; }
+        [DataMember]
+        public int AccountLevelID { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public string Username { get; set; }
+        [DataMember]
+        public string Password { get; set; }
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public DateTime Created { get; set; }
+
+        public Account(int iDAccount, int accountLevelID, string email, string username, string password, string firstName, string lastName, DateTime created)
+        {
+            IDAccount = iDAccount;
+            AccountLevelID = accountLevelID;
+            Email = email;
+            Username = username;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            Created = created;
+        }
+
+        public Account(int accountLevelID, string email, string username, string password, string firstName, string lastName, DateTime created)
+        {
+            AccountLevelID = accountLevelID;
+            Email = email;
+            Username = username;
+            Password = password;
+            FirstName = firstName;
+            LastName = lastName;
+            Created = created;
+        }
+
+        public Account()
+        {
+        }
+    }
+}
