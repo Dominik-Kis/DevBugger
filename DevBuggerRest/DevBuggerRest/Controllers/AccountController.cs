d2976873315a6d5e7a4081d4ad06998b2d333522
IvanMatkovic <ivanskola314@gmail.com>
2022-04-09 12:43:50 +0200

    Made procedures, next step tesing

--- DevBuggerRest/DevBuggerRest/Controllers/AccountController.cs
+++ DevBuggerRest/DevBuggerRest/Controllers/AccountController.cs
@@ -76,26 +76,126 @@ namespace DevBuggerRest.Controllers
                 command.Parameters.Add(new SqlParameter(ID, SqlDbType.Int));
                 command.Parameters[ID].Value = id;
 
-                    myConnection.Open();
-                    using (SqlDataReader oReader = command.ExecuteReader())
+                myConnection.Open();
+                using (SqlDataReader oReader = command.ExecuteReader())
+                {
+                    if (oReader.Read())
                     {
-                        if (oReader.Read())
-                        {
-                            acc.IDAccount = int.Parse(oReader[ID_ACCOUNT].ToString());
-                            acc.AccountLevelID = int.Parse(oReader["AccountLevelID"].ToString());
-                            acc.Email = oReader["Email"].ToString();
-                            acc.Username = oReader["Username"].ToString();
-                            acc.Password = oReader["Password"].ToString();
-                            acc.FirstName = oReader["FirstName"].ToString();
-                            acc.LastName = oReader["LastName"].ToString();
-                            acc.Created = DateTime.Parse(oReader["Created"].ToString());
-                        }
-
-                        myConnection.Close();
+                        acc.IDAccount = int.Parse(oReader[ID_ACCOUNT].ToString());
+                        acc.AccountLevelID = int.Parse(oReader["AccountLevelID"].ToString());
+                        acc.Email = oReader["Email"].ToString();
+                        acc.Username = oReader["Username"].ToString();
+                        acc.Password = oReader["Password"].ToString();
+                        acc.FirstName = oReader["FirstName"].ToString();
+                        acc.LastName = oReader["LastName"].ToString();
+                        acc.Created = DateTime.Parse(oReader["Created"].ToString());
                     }
-                
+
+                    myConnection.Close();
+                }
+
             }
             return acc;
         }
+
+
+        //http://localhost:52999/api/Account/UpdateAccount/1
+        [Route("[action]/{id}")]
+        [HttpPost]
+        public void UpdateAccount(Account account)
+        {
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                myConnection.Open();
+                using (SqlCommand command = myConnection.CreateCommand())
+                {
+                    command.CommandText = "UpdateAccount";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("IDAccount", account.IDAccount);
+                    command.Parameters.AddWithValue("Username", account.Username);
+                    command.Parameters.AddWithValue("Password", account.Password);
+                    command.Parameters.AddWithValue("FirstName", account.FirstName);
+                    command.Parameters.AddWithValue("LastName", account.LastName);
+                    command.ExecuteNonQuery();
+
+                }
+            }
+        }
+
+
+
+        //http://localhost:52999/api/Account/LoginAccount/1
+        [Route("[action]/{id}")]
+        [HttpPost]
+        public Account LoginAccount(string email, string password)
+        {
+            Account acc = new Account();
+            using (SqlConnection myConnection = new SqlConnection(con))
+            {
+                var command = new SqlCommand("loginAccount", myConnection);
+
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@Email", SqlDbType.NVarChar));
+                command.Parameters["@Email"].Value = email;
+                command.Parameters.Add(new SqlParameter("@Password", SqlDbType.NVarChar));
+                command.Parameters["@Password"].Value = password;
+
+                myConnection.Open();
+                using (SqlDataReader oReader = command.ExecuteReader())
+                {
+                    if (oReader.Read())
+                    {
+                        acc.IDAccount = int.Parse(oReader[ID_ACCOUNT].ToString());
+                        acc.AccountLevelID = int.Parse(oReader["AccountLevelID"].ToString());
+                        acc.Email = oReader["Email"].ToString();
+                        acc.Username = oReader["Username"].ToString();
+                        acc.Password = oReader["Password"].ToString();
+                        acc.FirstName = oReader["FirstName"].ToString();
+                        acc.LastName = oReader["LastName"].ToString();
+                        acc.Created = DateTime.Parse(oReader["Created"].ToString());
+                    }
+
+                    myConnection.Close();
+                }
+
+            }
+            return acc;
+        }
+
+
+        //http://localhost:52999/api/Account/CreatePerson/1
+        [Route("[action]/{id}")]
+        [HttpPost]
+        public void CreatePerson(Account account)
+        {
+            using (SqlConnection sqlConnection = new SqlConnection(con))
+            {
+                sqlConnection.Open();
+                using (SqlCommand cmd = sqlConnection.CreateCommand())
+                {
+                    cmd.CommandText = "createAccount";
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@AccountLevelID", account.AccountLevelID);
+                    cmd.Parameters.AddWithValue("@Email", account.Email);
+                    cmd.Parameters.AddWithValue("@Username", account.Username);
+                    cmd.Parameters.AddWithValue("@Password", account.Password);
+                    cmd.Parameters.AddWithValue("@Firstname", account.FirstName);
+                    cmd.Parameters.AddWithValue("@Lastname", account.LastName);
+                    /*
+                    SqlParameter id = new SqlParameter(
+                            IdPersonParam,
+                            System.Data.SqlDbType.Int)
+                    {
+                        Direction = System.Data.ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(id);
+                    */
+                    cmd.ExecuteNonQuery();
+                    //person.IDPerson = (int)id.Value;
+                }
+            }
+        }
+
+
     }
 }
