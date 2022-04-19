139261d22bfead1398b048a959f59aa316cd0b15
Dominik Kiš <dkis@racunarstvo.hr>
2022-04-19 13:46:54 +0200

    added LoginWindow and DAL

---
+++ DevBuggerDesktop/DevBuggerDesktop/DAL/RepoFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevBuggerDesktop.DAL
+{
+    public class RepoFactory
+    {
+        private static AccountRepository accountRepository { get; set; }
+        public static AccountRepository getAccountRepo()
+        {
+            if (accountRepository == null)
+            {
+                accountRepository = new AccountRepository();
+            }
+            return accountRepository;
+        }
+    }
+}
