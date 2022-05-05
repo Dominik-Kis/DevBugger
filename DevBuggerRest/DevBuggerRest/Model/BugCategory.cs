a5bd3151c52a35a98a0bb7cd8117f86501f4330b
IvanMatkovic <ivanskola314@gmail.com>
2022-05-05 23:12:24 +0200

    Added bug report classes

---
+++ DevBuggerRest/DevBuggerRest/Model/BugCategory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace DevBuggerRest.Model
+{
+    [DataContract]
+    public class BugCategory
+    {
+        [DataMember(Order = 0)]
+        public int IDCategory { get; set; }
+        [DataMember(Order = 1)]
+        public string Name { get; set; }
+        [DataMember(Order = 2)]
+        public string Description { get; set; }
+
+        public BugCategory(int iDCategory, string name, string description)
+        {
+            IDCategory = iDCategory;
+            Name = name;
+            Description = description;
+        }
+
+        public BugCategory()
+        {
+        }
+    }
+}
---
+++ DevBuggerRest/DevBuggerRest/Model/BugReportImage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace DevBuggerRest.Model
+{
+    [DataContract]
+    public class BugReportImage
+    {
+        [DataMember(Order = 0)]
+        public int IDBugReportImage { get; set; }
+        [DataMember(Order = 1)]
+        public int BugReportID { get; set; }
+        [DataMember(Order = 2)]
+        public byte[] Image { get; set; }
+
+    }
+}
